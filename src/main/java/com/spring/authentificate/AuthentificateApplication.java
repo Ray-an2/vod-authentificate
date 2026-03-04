3125abad5a2438a3d8f3cf3fc642dc120c97598d
Ray-an2 <brossardrayan.rb@gmail.com>
2026-03-04 11:13:09 +0100

    Ajout du service authentificate

---
+++ src/main/java/com/spring/authentificate/AuthentificateApplication.java
@@ -0,0 +1,13 @@
+package com.spring.authentificate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthentificateApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AuthentificateApplication.class, args);
+  }
+
+}
